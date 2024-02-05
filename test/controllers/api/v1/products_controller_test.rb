db95738e1edb90b5927026aae9eec618136cdcf1
aakashkushwah00 <aakash.kushwah@codiatic.com>
2024-02-05 18:51:17 +0530

    first commit done

---
+++ test/controllers/api/v1/products_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get api_v1_products_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get api_v1_products_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get api_v1_products_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get api_v1_products_edit_url
+    assert_response :success
+  end
+end
